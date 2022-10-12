d2f7dfc3a94d59ff53145eb02d41894245754a2a
pedominguezbr <pe.dominguez.br@gmail.com>
2022-10-12 16:48:46 -0500

    add files

---
+++ createfiles.py
@@ -0,0 +1,35 @@
+import os
+import errno
+
+#indice =4
+
+
+for indice in range(1, 10):
+    print(indice)
+    filename = f"C:\\Pervasivemind.net\\Lab\\argocd-example\\helm-values-files\\kuard{indice}\\kuard.yaml"
+    if not os.path.exists(os.path.dirname(filename)):
+        try:
+            os.makedirs(os.path.dirname(filename))
+        except OSError as exc: # Guard against race condition
+            if exc.errno != errno.EEXIST:
+                raise
+
+    with open(filename, "w") as f:
+        f.write(f'''yotpo:
+    deploymentName: kuard-test{indice}
+    chartName: onechart
+    chartPath: https://chart.onechart.dev/
+    chartVersion: v0.41.0
+    namespace: infra
+
+    replicaCount: 1
+    image:
+    repository: gcr.io/kuar-demo/kuard-amd64
+    tag: blue
+
+    containerPort: 8080
+    imagePullSecrets: []
+
+    ## Configure args passed to Telegraf containers
+    args: []
+        ''')
